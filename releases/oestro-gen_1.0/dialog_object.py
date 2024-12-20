37a2a202f5767c475616360b10e61f6c74282c50
Damien <dmnbrunstein@gmail.com>
2024-12-20 12:14:08 +0100

    ajout try catch pour l instantiation d une scene_edit_window dans dialog_object

--- releases/oestro-gen_1.0/dialog_object.py
+++ releases/oestro-gen_1.0/dialog_object.py
@@ -88,7 +88,10 @@ def build_ui_part(descendant,index_x,index_y, canvas):
         # print('Got object click', event.x, event.y)
         # print(event.widget.find_closest(event.x, event.y))
         if descendant.get_model_controller().get_current_project():
-            scene_window = SceneEditWindow(Toplevel(descendant.get_model_controller().get_current_window()),descendant)
+            try:
+                scene_window = SceneEditWindow(Toplevel(descendant.get_model_controller().get_current_window()),descendant)
+            except ValueError:
+                print("ERROR : Multiple scene_edit_window opened at the same time !")
         else:
             descendant.get_model_controller().broadcast_message("No project selected","error")
 
